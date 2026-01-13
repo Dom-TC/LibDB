0dc98585c310fe50ef8d7c5e385f1574256de29b
Dom Chester <dom.chester@me.com>
2026-01-13 19:02:58 +0000

    Move outdated code
    Signed-off-by: Dom Chester <dom.chester@me.com>

--- libdb/blueprints/__init__.py
+++ libdb/blueprints/__init__.py
@@ -1,6 +1,3 @@
 """Our various blueprints."""
 
-from . import library, manage
 
-library_blueprint = library.bp
-manage_blueprint = manage.bp
--- libdb/forms/__init__.py
+++ libdb/forms/__init__.py
@@ -1,7 +1,3 @@
 """Various Forms."""
 
-from . import add_author, add_book, search
 
-SearchForm = search.SearchForm
-AddAuthorForm = add_author.AddAuthorForm
-AddBookForm = add_book.AddBookForm
